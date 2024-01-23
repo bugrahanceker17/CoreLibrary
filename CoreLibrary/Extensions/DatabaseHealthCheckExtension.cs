47be5e1f0b5dd4f2305889f17d4aa3e0db865e82
Bugrahan <bugrahanceker17@gmail.com>
2024-01-23 13:02:55 +0300

    added db health check

---
+++ CoreLibrary/Extensions/DatabaseHealthCheckExtension.cs
@@ -0,0 +1,29 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CoreLibrary.Extensions;
+
+public static class DatabaseHealthCheckExtension
+{
+    public static void DbHealthCheckConfigure(this IServiceCollection services, string connectionString)
+    {
+            services.AddHealthChecks()
+            .AddSqlServer(
+                connectionString: connectionString,
+                healthQuery: "SELECT 1",
+                name: "MS SQL Server Check",
+                failureStatus: HealthStatus.Unhealthy | HealthStatus.Degraded,
+                tags: new string[] { "db", "sql", "sqlserver" });
+    }
+
+    public static void DbHealthCheckRegister(this IApplicationBuilder app)
+    {
+        app.UseHealthChecks("/health", new HealthCheckOptions
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+    }
+}
